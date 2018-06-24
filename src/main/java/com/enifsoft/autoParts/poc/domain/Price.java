e9a9dbf8ff64e87eaf9dd3ab89451050776f110f
Oleksii <bgeota@gmail.com>
2018-06-24 15:45:19 +0300

    create class Price

---
+++ src/main/java/com/enifsoft/autoParts/poc/domain/Price.java
@@ -0,0 +1,30 @@
+package com.enifsoft.autoParts.poc.domain;
+
+public class Price {
+    private int partId;
+    private int brandId;
+    private String brand;
+    private int partNumber;
+    private int partNumberShort;
+    private String partDescriptionRus;
+    private Double price;
+    private String currency;
+    private int quantity;
+    private String quantityType;
+    private String priceLogo;
+    private int Weight;
+    private String priceDescrShort;
+    private String priceDescrLong;
+    private String deliveryType;
+    private int deliveryDays;
+    private String deliveryDate;
+    private Double deliveryPercent;
+    private String priceChangeDate;
+    private String DamagedFlag;
+    private String usedFlag;
+    private String originalFlag;
+    private String oldPartNumberFlag;
+    private String returnFlag;
+    private String priceFinalFlag;
+    private int group;
+}
