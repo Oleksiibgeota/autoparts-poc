69d7195712daf76a231816ff0e03c49c404d6ac6
Oleksii <bgeota@gmail.com>
2018-06-24 15:48:04 +0300

    add get set Lombok

--- src/main/java/com/enifsoft/autoParts/poc/domain/Price.java
+++ src/main/java/com/enifsoft/autoParts/poc/domain/Price.java
@@ -1,5 +1,10 @@
 package com.enifsoft.autoParts.poc.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Price {
     private int partId;
     private int brandId;
