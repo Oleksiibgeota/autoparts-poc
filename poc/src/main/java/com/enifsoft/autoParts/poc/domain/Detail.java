2b3784e1fa3f86eba1e17ba2a4695e0da0ae12ee
Oleksii <bgeota@gmail.com>
2018-06-24 15:03:31 +0300

    first commit

---
+++ poc/src/main/java/com/enifsoft/autoParts/poc/domain/Detail.java
@@ -0,0 +1,20 @@
+package com.enifsoft.autoParts.poc.domain;
+
+public class Detail {
+    private String brand;
+    private int number;
+    private String name;
+//    private Price price;
+//    private Currency currency;
+    private String supplierCode;
+    private int quantity;
+//        <Quantity>1</Quantity>
+//        <SupplierCode>GLIR</SupplierCode>
+//        <Weight>6.141</Weight>
+//        <DeliveryType>LOCAL</DeliveryType>
+//        <DeliveryTime>15</DeliveryTime>
+//        <DeliveryDate>2018-07-09 10:00:00</DeliveryDate>
+//        <DamagedFlag>N</DamagedFlag>
+//        <UsedFlag>N</UsedFlag>
+//        <RestoredFlag>N</RestoredFlag>
+}
